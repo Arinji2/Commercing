f5c8119a41a09e6fd84f08b6b33e19b399a7fb8f
Arinji2 <87296817+Arinji2@users.noreply.github.com>
2022-09-28 10:07:45 +0530

    Nav #4

--- src/components/nav.js
+++ src/components/nav.js
@@ -6,24 +6,26 @@ class Navbar extends Component {
   state = {};
   handleBurgerClick() {
     let menu = document.getElementById("navbar");
-    let logo = document.getElementById("logo");
+    let menuParent = document.getElementById("menuParent");
     let button = document.getElementById("burger-button");
-    if (menu.style.display !== "flex") {
-      menu.style.display = "flex";
+    if (menu.style.opacity !== "1") {
+      menu.style.opacity = "1";
+      menuParent.style.backgroundColor = "rgb(23 23 23 / var(--tw-bg-opacity))";
     } else {
-      menu.style.display = "none";
+      menu.style.opacity = "0";
+      menuParent.style.backgroundColor = "black";
     }
     button.classList.toggle("fa-rotate-90");
   }
   render() {
     return (
       <div>
-        <div className="bg-neutral-900 text-white text-2xl md:text-3xl pr-3 pl-3 fixed top-0 w-screen">
+        <div
+          className="bg-black md:bg-neutral-900  text-white text-2xl md:text-3xl pr-3 pl-3 fixed top-0 w-screen"
+          id="menuParent"
+        >
           <ul className="sticky flex flex-col md:flex-row md:justify-between items-start md:items-center p-2">
-            <li
-              id="logo"
-              className="bg-neutral-900 flex items-center h-24 w-24"
-            >
+            <li id="logo" className="bg-inherit flex items-center h-24 w-24">
               <img
                 src={logo}
                 alt={logo}
@@ -31,7 +33,7 @@ class Navbar extends Component {
               ></img>
             </li>
             <div
-              className="sticky hidden md:flex flex-col md:flex-row md:justify-between items-start md:items-center p-2 w-screen md:pl-7"
+              className="sticky flex opacity-0 md:opacity-100 md:flex flex-col md:flex-row md:justify-between items-end pr-10 md:items-center p-2 w-screen md:pl-7 transition-opacity ease-in-out duration-500"
               id="navbar"
             >
               <li className="hover:text-yellow-400 transition-all ease-in-out duration-300 hover:scale-100 md:scale-75  hover:cursor-pointer">
@@ -43,7 +45,7 @@ class Navbar extends Component {
               <li className="hover:text-yellow-400 transition-all ease-in-out duration-300 hover:scale-100 md:scale-75  hover: hover:cursor-pointer">
                 Team
               </li>
-              <li className="hover:text-yellow-400 transition-all ease-in-out duration-300 hover:scale-100 md:scale-75  hover: hover:cursor-pointer mr-10  pr-3">
+              <li className="hover:text-yellow-400 transition-all ease-in-out duration-300 hover:scale-100 md:scale-75  hover: hover:cursor-pointer md:mr-10  md:pr-3">
                 Contact
               </li>
             </div>
